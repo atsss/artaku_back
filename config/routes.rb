3ec1e64bbfc6d33341c3c2c132082a536e4e689f
atsss <atsushi0623itoh@gmail.com>
2021-02-12 23:33:15 -0600

    :+1: set up action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  mount ActionCable.server => '/cable'
   post '/graphql', to: 'graphql#execute'
 end
